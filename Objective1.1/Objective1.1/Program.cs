875c4f6e4e1f480ad441a45936e1facb9474a6f6
VICTOR <victor.toderica@gmail.com>
2019-01-05 01:09:15 +0200

    Importing project

---
+++ Objective1.1/Objective1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objective1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
