bb338600405349141d23eb875ee11afdf443954c
VICTOR <victor.toderica@gmail.com>
2019-01-05 01:55:47 +0200

    test 2

--- Objective1.1/Objective1.1/Program.cs
+++ Objective1.1/Objective1.1/Program.cs
@@ -10,7 +10,7 @@ namespace Objective1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World ");
         }
     }
 }
