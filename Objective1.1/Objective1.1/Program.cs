686017517134c3057b548b5fd4845d51c469ca13
VICTOR <victor.toderica@gmail.com>
2019-02-11 20:02:58 +0200

    add threads

--- Objective1.1/Objective1.1/Program.cs
+++ Objective1.1/Objective1.1/Program.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Objective1._1
 {
     class Program
     {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc {0}", i);
+                Thread.Sleep(0);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World ");
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work");
+                Thread.Sleep(0);
+            }
+            t.Join();
+            Console.ReadKey();
         }
     }
 }
