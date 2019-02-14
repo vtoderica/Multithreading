978ac3596421a84160df49800cd7a1ddf8bdbb5b
VICTOR <victor.toderica@gmail.com>
2019-02-14 22:38:21 +0200

    add some more code here 2

--- Objective1.1/Objective1.1/Program.cs
+++ Objective1.1/Objective1.1/Program.cs
@@ -68,41 +68,101 @@ namespace Objective1._1
         //}
         #endregion
         #region partea 4 - stop threads; thread static
-
         //every variable created in one tread it is unique and exist only in that thread 
         //normal global variables => all the treads will use the same variable
 
         // thread static => every single thread will have its own copy of it
 
-        [ThreadStatic]                          /// static thread => for variable static _field is created a copy for every thread 
-        public static int _field;
+        //[ThreadStatic]                          /// static thread => for variable static _field is created a copy for every thread 
+        //public static int _field;
+        //static void Main(string[] args)
+        //{
+        //    Thread t1 = new Thread(new ThreadStart(()=>
+        //    {
+        //        for (int i = 0; i < 10; i++)
+        //        {
+        //            _field++;
+        //            Console.WriteLine("Thread A : {0}", _field);
+        //        }
+        //    }));
+        //    t1.Start();
+        //    t1.Join();
+        //    Thread t2 = new Thread(new ThreadStart(() =>
+        //    {
+        //        for (int i = 0; i < 10; i++)
+        //        {
+        //            _field++;
+        //            Console.WriteLine("Thread B : {0}", _field);
+        //        }
+        //    }));
+        //    t2.Start();
+        //    t2.Join();
+        //    Console.ReadKey();
+        //}
+        #endregion
+        #region partea 5  - queuing some work to the thread pool
+        // - new thread for many many requests (eg. On server side  => trouble(overload)) => the solution = threadpools
+        //static void Main(string[] args)
+        //{
+        //    //adds the work on this thread pool
+        //    //s is for state
+        //    ThreadPool.QueueUserWorkItem((s) =>
+        //    {
+        //        Console.WriteLine("Working on a thread from the thread pool");
+        //    });
+        //    Console.ReadLine();
+        //}
+        #endregion
+        #region partea 6 - Start working with TASK
+        //public static void ThreadMethod()
+        //{
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        Console.Write('*');
+        //    }
+
+        //}
+        //tasks use the thread pool
+        //tasks = managed thread pool threads; tasks can return values (return an task for ex)
+        //static void Main(string[] args)
+        //{
+        //    //Action = predefined delegates has no param and returns nothing
+        //    //Task t = Task.Run(() =>
+        //    //{
+        //    //    for (int i = 0; i < 100; i++)
+        //    //    {
+        //    //        Console.Write('*');
+        //    //    }
+        //    //});
+        //    Task t = Task.Run(action:ThreadMethod);
+        //    t.Wait();
+        //    Console.ReadKey();
+        //}
+        #endregion
+        #region partea 7 Tasks that returns values
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(new ThreadStart(()=>
+            Task<int> t = Task.Run(() =>
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    _field++;
-                    Console.WriteLine("Thread A : {0}", _field);
-                }
-            }));
-            t1.Start();
-            t1.Join();
-            Thread t2 = new Thread(new ThreadStart(() =>
+                return 42;
+            }
+            ).ContinueWith((taskInt) =>
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    _field++;
-                    Console.WriteLine("Thread B : {0}", _field);
-                }
-            }));
-            t2.Start();
-            t2.Join();
+                return taskInt.Result * 2;
+            }
+            );
+
+            t = t.ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            }
+            ); 
+            Console.WriteLine(t.Result); //t do not have to wait. using Result implies that we have to wait until the task is ended
             Console.ReadKey();
+            //after task finishes we can do continuations to do some more work
         }
         #endregion
-
-        #region partea 5  - queuing some work to the thread pool
+        #region partea 8
 
         #endregion
     }
